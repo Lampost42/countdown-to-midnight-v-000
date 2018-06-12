7b1b1564c576d19da87362a3d53aa28c172eb009
Trevor Russell <truss.42@gmail.com>
2018-06-12 21:25:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
-def countdown(number)
+def LOOPS(number)
   while number > 0
     puts "#{number} SECOND(S)"
     number -= 1
-    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
