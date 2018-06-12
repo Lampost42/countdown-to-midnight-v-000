e4470fa8c6d309c1c1b1a6768c6447880e340aa8
Trevor Russell <truss.42@gmail.com>
2018-06-12 21:23:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,9 @@
 #write your code here
 
 def countdown(number)
-  counter = number
-  while counter > 0
+  while number > 0
     puts "#{number} SECOND(S)"
-    counter -= 1
+    number -= 1
     sleep(1)
   end
   return "HAPPY NEW YEAR!"
